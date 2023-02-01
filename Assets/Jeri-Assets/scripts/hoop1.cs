e6e1f20d1fe68427dc69ba7fb849027f03db1f34
JerryChili <jeri.juntunen@gmail.com>
2023-02-01 20:01:53 +0200

    a

--- Assets/Jeri-Assets/scripts/hoop1.cs
+++ Assets/Jeri-Assets/scripts/hoop1.cs
@@ -4,12 +4,19 @@ using UnityEngine;
 
 public class hoop1 : MonoBehaviour
 {
+    int hoop1score = 0;
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    private void OnTriggerEnter(Collider backboard)
+    {
+        hoop1score++;
+        Debug.Log("Ball passed through hoop 1");
+    }
     // Update is called once per frame
     void Update()
     {
--- Assets/Jeri-Assets/scripts/hoop2.cs
+++ Assets/Jeri-Assets/scripts/hoop2.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class hoop2 : MonoBehaviour
 {
+    int hoop2score = 0;
+
     // Start is called before the first frame update
     void Start()
     {
         
+    }
+
+    private void OnTriggerEnter(Collider backboard2)
+    {
+        hoop2score++;
+        Debug.Log("Ball passed through hoop 2");
     }
 
     // Update is called once per frame
